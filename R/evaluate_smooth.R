aee3dc71d8973966cc5065cb107b7c32f667929e
Gavin Simpson <ucfagls@gmail.com>
2017-06-27 20:11:54 -0600

    evaluate smooth now accepts a newdata argument

--- R/evaluate_smooth.R
+++ R/evaluate_smooth.R
@@ -1,13 +1,16 @@
 ##' Evaluate a smooth
 ##'
-##' Evaluate a smooth at a grid of evenly spaced value over the range of the covariate associated with the smooth.
+##' Evaluate a smooth at a grid of evenly spaced value over the range of the covariate associated with the smooth. Alternatively, a set of points at which the smooth should be evaluated can be supplied.
 ##'
-##' @param object an object of class `"gam"` or `"gamm"`
-##' @param term character; a single term to evaluate
-##' @param n numeric; the number of points over the range of the covariate at which to evaluate the smooth
+##' @param object an object of class `"gam"` or `"gamm"`.
+##' @param term character; a single term to evaluate.
+##' @param n numeric; the number of points over the range of the covariate at which to evaluate the smooth.
+##' @param newdata a vector or data frame of points at which to evaluate the smooth.
+##' @param unconditional logical; should confidence intervals include the uncertainty due to smoothness selection? If `TRUE`, the corrected Bayesian covariance matrix will be used.
 ##' @param inc.mean logical; should the uncertainty in the model constant term be included in the standard error of the evaluate values of the smooth? Currently not implemented.
 ##'
 ##' @importFrom mgcv PredictMat
+##' @importFrom stats setNames
 ##'
 ##' @export
 ##'
@@ -18,7 +21,8 @@
 ##' mod <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
 ##'
 ##' head(evaluate_smooth(mod, "x1"))
-evaluate_smooth <- function(object, term, n = 200, inc.mean = FALSE) {
+evaluate_smooth <- function(object, term, n = 200, newdata = NULL,
+                            unconditional = FALSE, inc.mean = FALSE) {
     ## simplify GAMM objects
     if (is.gamm(object)) {
         object <- object$gam
@@ -29,9 +33,22 @@ evaluate_smooth <- function(object, term, n = 200, inc.mean = FALSE) {
         term <- term[1L]
     }
     term <- select_terms(object, term) # match the supplied term
-    smooth <- get_smooth(object, term)
-    newx <- datagen(smooth, n = n, data = object$model)[, "x", drop = FALSE]
-    names(newx) <- term
+    smooth <- get_smooth(object, term) # extract the mgcv.smooth object
+    newx <- if (is.null(newdata)) {
+        setNames(datagen(smooth, n = n,
+                         data = object$model)[, "x", drop = FALSE],
+                 term)
+    } else if (is.data.frame(newdata)) { # data frame; select out term
+        if (!term %in% names(newdata)) {
+            stop(paste("Smooth", term, "not found in 'newdata'."))
+        }
+        newdata[, term, drop = FALSE]
+    } else if (is.numeric(newdata)) {   # vector; coerce to data frame
+        setNames(data.frame(newdata), term)
+    } else {                            # object we can't handle; bail out
+        stop("'newdata', if supplied, must be a numeric vector or a data frame.")
+    }
+
     X <- PredictMat(smooth, newx)               # prediction matrix
     start <- smooth$first.para
     end <- smooth$last.para
@@ -39,11 +56,11 @@ evaluate_smooth <- function(object, term, n = 200, inc.mean = FALSE) {
     coefs <- coef(object)[para.seq]
     fit <- X %*% coefs
 
-    Vp <- object$Vp         # variance covariance matrix of parameters
+    V <- get_vcov(object, unconditional = unconditional, term = term)
     if (isTRUE(inc.mean)) {
         stop("'inc.mean == TRUE' situation not currently supported")
     } else {
-        rs <- rowSums((X %*% Vp[para.seq, para.seq, drop = FALSE]) * X)
+        rs <- rowSums((X %*% V) * X)
         se.fit <- sqrt(pmax(0, rs))
     }
 
