eb885a23ca29c7d167012b1baab2ad21001ba5b7
Gavin Simpson <ucfagls@gmail.com>
2018-01-04 21:19:28 -0600

    document remaining params

--- R/draw-methods.R
+++ R/draw-methods.R
@@ -17,8 +17,19 @@
 ##'
 ##' @title Plot a 2D smooth
 ##' @param object an object, the result of a call to [tsgam::evaluate_smooth()].
+##' @param what character; plot the estimated smooth (`"estimate"`) or its
+##'   standard error (`"se"`).
 ##' @param contour logical; should contours be draw on the plot using
 ##'   [ggplot2::geom_contour()].
+##' @param xlab character or expression; the label for the x axis. If not
+##'   supplied, a suitable label will be generated from `object`.
+##' @param ylab character or expression; the label for the y axis. If not supplied, a suitable label will be generated from `object`.
+##' @param title character or expression; the title for the plot. See
+##'   `ggplot2::labs()`.
+##' @param subtitle character or expression; the subtitle for the plot. See
+##'   `ggplot2::labs()`.
+##' @param caption character or expression; the plot caption. See
+##'    `ggplot2::labs()`.
 ##' @param ... arguments passed to other methods.
 ##'
 ##' @return A `ggplot2` object
@@ -46,17 +57,32 @@
 ##'
 ##' sm <- evaluate_smooth(m2, "s(x,z)", n = 100)
 ##' draw(sm)
-`draw.evaluated_2d_smooth` <- function(object, contour = TRUE,
+##'
+##' ## how the standard error the smooth instead
+##' draw(sm, what = "se")
+`draw.evaluated_2d_smooth` <- function(object, what = c("estimate","se"),
+                                       contour = TRUE,
                                        xlab, ylab,
                                        title = NULL, subtitle = NULL,
                                        caption = NULL,
                                        ...) {
     smooth_vars <- names(object)[2:3]
+    what <- match.arg(what)
+    if (isTRUE(identical(what, "estimate"))) {
+        guide_title <- levels(object[["smooth"]])
+        plot_var <- "est"
+        guide_limits <- c(-1, 1) * max(abs(object[[plot_var]]))
+    } else {
+        guide_title <- bquote(SE * (.(levels(object[["smooth"]]))))
+        plot_var <- "se"
+        guide_limits <- range(object[["se"]])
+    }
+
     plt <- ggplot(object, aes_string(x = smooth_vars[1], y = smooth_vars[2])) +
-        geom_raster(mapping = aes_string(fill = "est"))
+        geom_raster(mapping = aes_string(fill = plot_var))
 
     if (isTRUE(contour)) {
-        plt <- plt + geom_contour(mapping = aes_string(z = "est"))
+        plt <- plt + geom_contour(mapping = aes_string(z = plot_var))
     }
 
     ## default axis labels if none supplied
@@ -73,10 +99,10 @@
 
     ## Set the palette
     plt <- plt + scale_fill_distiller(palette = "RdBu", type = "div",
-                                      limits = c(-1, 1) * max(abs(object[["est"]])))
+                                      limits = guide_limits)
 
     ## add guide
-    plt <- plt + guides(fill = guide_colourbar(title = expression(hat(s))))
+    plt <- plt + guides(fill = guide_colourbar(title = guide_title))
 
     plt
 }
