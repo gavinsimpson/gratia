fe92a0eea3a930ee03f8d5bf657bbb5839829e0b
Gavin Simpson <ucfagls@gmail.com>
2018-01-13 14:18:44 -0600

    add the small water age depth measurements

---
+++ R/datasets.R
@@ -0,0 +1,20 @@
+##' Lead-210 age-depth measurements for Small Water
+##'
+##' A dataset containing lead-210 based age depth measurements for the SMALL1
+##' core from Small Water.
+##'
+##' The variables are as follows:
+##'
+##' * `Depth`
+##' * `Drymass`
+##' * `Date`
+##' * `Age`
+##' * `Error`
+##' * `SedAccRate`
+##' * `SedPerCentChange`
+##'
+##' @format A data frame with 12 rows and 7 variables.
+##' @keywords data
+##' @name smallAges
+##' @docType data
+NULL
