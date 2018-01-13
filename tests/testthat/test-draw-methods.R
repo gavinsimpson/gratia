14f2d9a702e44d1add96a289b1605a54abc37c14
Gavin Simpson <ucfagls@gmail.com>
2018-01-13 12:34:53 -0600

    outline calls to draw methods rather tha inline them in vdiffr calls

--- tests/testthat/test-draw-methods.R
+++ tests/testthat/test-draw-methods.R
@@ -5,6 +5,7 @@ library("testthat")
 library("schoenberg")
 library("mgcv")
 library("ggplot2")
+library("vdiffr")
 theme_set(theme_grey())
 
 context("draw-methods")
@@ -14,7 +15,8 @@ test_that("draw.evaluated_1d_smooth() plots the smooth", {
     dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
     m1 <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
     sm <- evaluate_smooth(m1, "s(x2)")
-    vdiffr::expect_doppelganger("draw 1d smooth for selected smooth", draw(sm))
+    plt <- draw(sm)
+    expect_doppelganger("draw 1d smooth for selected smooth", plt)
 })
 
 test_that("draw.evaluated_2d_smooth() plots the smooth & SE", {
@@ -22,34 +24,43 @@ test_that("draw.evaluated_2d_smooth() plots the smooth & SE", {
     dat <- gamSim(2, n = 4000, dist = "normal", scale = 1, verbose = FALSE)
     m2 <- gam(y ~ s(x, z, k = 40), data = dat$data, method = "REML")
     sm <- evaluate_smooth(m2, "s(x,z)", n = 100)
-    vdiffr::expect_doppelganger("draw 2d smooth", draw(sm))
-    vdiffr::expect_doppelganger("draw std error of 2d smooth", draw(sm, show = "se"))
+    plt <- draw(sm)
+    expect_doppelganger("draw 2d smooth", plt)
+    plt <- draw(sm, show = "se")
+    expect_doppelganger("draw std error of 2d smooth", plt)
 })
 
 test_that("draw.gam() plots a simple multi-smooth AM", {
     set.seed(1)
     dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
     m1 <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
-    vdiffr::expect_doppelganger("draw simple multi-smooth AM", draw(m1))
-    vdiffr::expect_doppelganger("draw simple multi-smooth AM with fixed scales",
-                                draw(m1, scales = "fixed"))
+
+    plt <- draw(m1)
+    expect_doppelganger("draw simple multi-smooth AM", plt)
+
+    plt <- draw(m1, scales = "fixed")
+    expect_doppelganger("draw simple multi-smooth AM with fixed scales", plt)
 })
 
 test_that("draw.gam() plots an AM with a single 2d smooth", {
     set.seed(1)
     dat <- gamSim(2, n = 4000, dist = "normal", scale = 1, verbose = FALSE)
     m2 <- gam(y ~ s(x, z, k = 30), data = dat$data, method = "REML")
-    vdiffr::expect_doppelganger("draw AM with 2d smooth", draw(m2))
+
+    plt <- draw(m2)
+    expect_doppelganger("draw AM with 2d smooth", plt)
 })
 
 test_that("draw.gam() plots an AM with a single factor by-variable smooth", {
     set.seed(1)
     dat <- gamSim(4, verbose = FALSE)
     m3 <- gam(y ~ fac + s(x2, by = fac) + s(x0), data = dat)
-    vdiffr::expect_doppelganger("draw AM with factor by-variable smooth",
-                                draw(m3))
-    vdiffr::expect_doppelganger("draw AM with factor by-variable smooth with fixed scales",
-                                draw(m3, scales = "fixed"))
+
+    plt <- draw(m3)
+    expect_doppelganger("draw AM with factor by-variable smooth", plt)
+
+    plt <- draw(m3, scales = "fixed")
+    expect_doppelganger("draw AM with factor by-variable smooth with fixed scales", plt)
 })
 
 ## simulate date from y = f(x2)*x1 + error
@@ -57,13 +68,13 @@ dat <- gamSim(3, n = 400, verbose = FALSE)
 mod <- gam(y ~ s(x2, by = x1), data = dat)
 
 test_that("draw() works with continuous by", {
-    vdiffr::expect_doppelganger("draw AM with continuous by-variable smooth",
-                                draw(mod))
+    plt <- draw(mod)
+    expect_doppelganger("draw AM with continuous by-variable smooth", plt)
 })
 
 test_that("draw() works with continuous by and fixed scales", {
-    vdiffr::expect_doppelganger("draw AM with continuous by-var fixed scale",
-                                draw(mod, scales = "fixed"))
+    plt <- draw(mod, scales = "fixed")
+    expect_doppelganger("draw AM with continuous by-var fixed scale", plt)
 })
 
 test_that("draw() works with random effect smooths (bs = 're')", {
@@ -80,12 +91,15 @@ test_that("draw() works with random effect smooths (bs = 're')", {
 
     sm <- evaluate_smooth(rm1, "s(fac)")
     expect_s3_class(sm, "evaluated_re_smooth")
-    vdiffr::expect_doppelganger("draw.evaluated_re_smooth",
-                                draw(sm))
-    vdiffr::expect_doppelganger("draw.gam model with ranef smooth",
-                                draw(rm1, ncol = 3))
-    vdiffr::expect_doppelganger("draw.gam model with ranef smooth fixed scales",
-                                draw(rm1, ncol = 3, scales = "fixed"))
+
+    p1 <- draw(sm)
+    expect_doppelganger("draw.evaluated_re_smooth", p1)
+
+    p2 <- draw(rm1, ncol = 3)
+    expect_doppelganger("draw.gam model with ranef smooth", p2)
+
+    p3 <- draw(rm1, ncol = 3, scales = "fixed")
+    expect_doppelganger("draw.gam model with ranef smooth fixed scales", p3)
 })
 
 test_that("draw() with random effect smooths (bs = 're') & factor by variable ", {
@@ -105,10 +119,10 @@ test_that("draw() with random effect smooths (bs = 're') & factor by variable ",
 
     sm <- evaluate_smooth(rm2, "s(ranef)")
     expect_s3_class(sm, "evaluated_re_smooth")
-    ## vdiffr::expect_doppelganger("draw.evaluated_re_smooth with factor by",
-    ##                             draw(sm))
-    vdiffr::expect_doppelganger("draw.gam model with ranef smooth factor by",
-                                draw(rm2, ncol = 3))
-    vdiffr::expect_doppelganger("draw.gam model with ranef smooth factor by fixed scales",
-                                draw(rm2, ncol = 3, scales = "fixed"))
+    p1 <- draw(sm)
+    ## vdiffr::expect_doppelganger("draw.evaluated_re_smooth with factor by", p1)
+    p2 <- draw(rm2, ncol = 3)
+    expect_doppelganger("draw.gam model with ranef smooth factor by", p2)
+    p3 <- draw(rm2, ncol = 3, scales = "fixed")
+    expect_doppelganger("draw.gam model with ranef smooth factor by fixed scales", p3)
 })
