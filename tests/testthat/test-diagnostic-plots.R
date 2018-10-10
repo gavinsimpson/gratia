e24f68fff999cb5820f66139c3b0d44e39d3b080
Gavin Simpson <ucfagls@gmail.com>
2018-10-09 23:07:00 -0600

    clean up

--- tests/testthat/test-diagnostic-plots.R
+++ tests/testthat/test-diagnostic-plots.R
@@ -15,8 +15,6 @@ set.seed(2)
 ## simulate some data...
 dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
 mod <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat)
-## run some basic model checks, including checking
-## smoothing basis dimensions...
 
 test_that("appraise() works", {
     plt <- appraise(mod)
