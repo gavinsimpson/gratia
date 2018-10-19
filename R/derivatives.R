cb535f9aa2208115d71465e0ef77d9add8401a2b
Gavin Simpson <ucfagls@gmail.com>
2018-10-18 23:23:04 -0600

    fill out some more of the skeleton

--- R/derivatives.R
+++ R/derivatives.R
@@ -3,6 +3,8 @@
 ##' @param object an R object to compute derivatives for.
 ##' @param ... arguments passed to other methods.
 ##'
+##' @author Gavin L. Simpson
+##'
 ##' @export
 `derivatives` <- function(object, ...) {
     UseMethod("derivatives")
@@ -20,15 +22,25 @@
 
 ##' @rdname derivatives
 ##'
+##' @param newdata a data frame containing the values of the model covariates
+##'   at which to evaluate the first derivatives of the smooths.
 ##' @param order numeric; the order of derivative.
 ##' @param type character; the type of finite difference used. One of `"left"`,
 ##'   `"right"`, or `"centre"` (or `"center"`).
 ##'
 ##' @export
-`derivatives.gam` <- function(object, order = c(1, 2),
-                              type = c("left", "right", "centre", "center"), ...) {
+`derivatives.gam` <- function(object, newdata, order = c(1, 2),
+                              type = c("left", "right", "centre", "center"),
+                              n = 200, eps = 1e-7, ...) {
     type <- match.arg(type)
     if (type == "center") {
         type <- "centre"
     }
+}
+
+##' @rdname derivatives
+##'
+##' @export
+`derivatives.gamm` <- function(object, ...) {
+    derivatives(object[["gam"]], ...)
 }
