de1aa5ae48560d01cb088f2e1dab7bf377c3b1dd
Gavin Simpson <ucfagls@gmail.com>
2018-10-18 22:34:22 -0600

    add some tests for appraise and associated plot methods

--- tests/testthat/test-diagnostic-plots.R
+++ tests/testthat/test-diagnostic-plots.R
@@ -20,3 +20,15 @@ test_that("appraise() works", {
     plt <- appraise(mod)
     expect_doppelganger("appraise diagnostic plots", plt)
 })
+
+test_that("appraise() fails if n_bins not numeric or one of character options", {
+    msg <- paste("'arg' should be one of",
+                 paste(dQuote(c('sturges', 'scott', 'fd')), collapse = ", "))
+    expect_error(appraise(mod, n_bins = "foo"), msg, fixed = TRUE)
+})
+
+test_that("residuals_hist_plot fails if non-numeric n_bins doesn't match character options", {
+    msg <- paste("'arg' should be one of",
+                 paste(dQuote(c('sturges', 'scott', 'fd')), collapse = ", "))
+    expect_error(residuals_hist_plot(mod, n_bins = "foo"), msg, fixed = TRUE)
+})
