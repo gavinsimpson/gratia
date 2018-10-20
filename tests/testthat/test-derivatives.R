52334ffd6f2debfd2104dec92cd310af78337894
Gavin Simpson <ucfagls@gmail.com>
2018-10-20 16:37:38 -0600

    add a test of a gamm() model; add test for failure in default method

--- tests/testthat/test-derivatives.R
+++ tests/testthat/test-derivatives.R
@@ -11,6 +11,13 @@ set.seed(42)
 dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
 mod <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
 
+test_that("derivatives fails for an unknown object", {
+    df <- data.frame(a = 1:10, b = 1:10)
+    expect_error(derivatives(df),
+                 "Don't know how to calculate derivatives for <data.frame>",
+                 fixed = TRUE)
+})
+
 test_that("derivatives() fails with inappropriate args", {
     expect_error(derivatives(mod, type = "foo"),
                  paste("'arg' should be one of",
@@ -79,6 +86,29 @@ test_that("derivatives() throws errors if order == 2L", {
                  "2nd derivatives not yet implemented.", fixed = TRUE)
 })
 
+mod <- gamm(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
+
+test_that("derivatives() returns derivatives for all smooths in a GAMM", {
+    expect_silent(df <- derivatives(mod))
+    expect_s3_class(df, "derivatives")
+    expect_s3_class(df, "tbl_df")
+    expect_named(df, c("smooth","var","data","derivative","se"))
+
+    expect_silent(df <- derivatives(mod, type = "forward"))
+    expect_s3_class(df, "derivatives")
+    expect_s3_class(df, "tbl_df")
+    expect_named(df, c("smooth","var","data","derivative","se"))
+
+    expect_silent(df <- derivatives(mod, type = "backward"))
+    expect_s3_class(df, "derivatives")
+    expect_s3_class(df, "tbl_df")
+    expect_named(df, c("smooth","var","data","derivative","se"))
+
+    expect_silent(df <- derivatives(mod, type = "central"))
+    expect_s3_class(df, "derivatives")
+    expect_s3_class(df, "tbl_df")
+    expect_named(df, c("smooth","var","data","derivative","se"))
+})
 
 ## confint methods for by variables
 set.seed(2)
