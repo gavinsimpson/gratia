ef7cfddf54821ebfef488b9511bc34ce5105526c
Gavin Simpson <ucfagls@gmail.com>
2018-10-23 23:38:25 -0600

    add pointwise interval capacity for derivatives object

--- R/add-methods.R
+++ R/add-methods.R
@@ -21,9 +21,32 @@
 ##' @rdname add_confint
 ##'
 ##' @export
+##'
+##' @importFrom tibble add_column
 `add_confint.derivatives` <- function(object,
                                       level = 0.95,
                                       type = c("confidence", "simultaneous"),
                                       ...) {
+    type <- match.arg(type)
 
+    if (type == "confidence") {
+        crit <- coverage_qnorm(level)
+        result <- add_column(object,
+                             lower = object[["derivative"]] - (crit * object[["se"]]),
+                             upper = object[["derivative"]] + (crit * object[["se"]]))
+    }
+
+    if (type == "simultaneous") {
+        stop("Simultaneous intervals not yet implemented for <derivatives>",
+             call. = FALSE)
+    }
+
+    result
+}
+
+`coverage_qnorm` <- function(level) {
+    if (level <= 0 || level >= 1 ) {
+        stop("Invalid 'level': must be 0 < level < 1", call. = FALSE)
+    }
+    qnorm((1 - level) / 2, lower.tail = FALSE)
 }
