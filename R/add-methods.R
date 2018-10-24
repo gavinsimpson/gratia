312caac88ce6c068b58399e62188ee8b862830db
Gavin Simpson <ucfagls@gmail.com>
2018-10-23 22:29:59 -0600

    add skeleton for add_confint plus default and derivatives methods

---
+++ R/add-methods.R
@@ -0,0 +1,29 @@
+##' Add confidence intervals to objects
+##'
+##' @param object
+##' @param ...
+`add_confint` <- function(object, ...) {
+    UseMethod("add_confint")
+}
+
+##' @rdname add_confint
+##'
+##' @export
+`add_confint.default` <- function(object, ...) {
+    stop("Don't know how to add confidence intervals to <",
+         class(object)[[1L]], ">", call. = FALSE)
+}
+
+##' @param level numeric; the coverage of the intervals
+##' @param type character; the type of interval to compute. One of `"confidence"`
+##'   for point-wise intervals, or `"simultaneous"` for simultaneous intervals.
+##'
+##' @rdname add_confint
+##'
+##' @export
+`add_confint.derivatives` <- function(object,
+                                      level = 0.95,
+                                      type = c("confidence", "simultaneous"),
+                                      ...) {
+
+}
