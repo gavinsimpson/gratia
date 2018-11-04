9861dd47e89030cc15dc2ad6dd4e25012ef17f6b
Gavin Simpson <ucfagls@gmail.com>
2018-11-04 16:52:44 -0600

    update tests

--- tests/testthat/test-simulate-methods.R
+++ tests/testthat/test-simulate-methods.R
@@ -13,6 +13,11 @@ dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
 m1 <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
 m2 <- gamm(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
 
+data(smallAges)
+smallAges$Error[1] <- 1.1
+sw <- scam(Date ~ s(Depth, k = 5, bs = "mpd"), data = smallAges,
+           weights = 1 / smallAges$Error, gamma = 1.4)
+
 test_that("simulate() works with a gam", {
     sims <- simulate(m1, nsim = 5, seed = 42)
     expect_identical(nrow(sims), 400L)
@@ -27,12 +32,38 @@ test_that("simulate() works with a gamm", {
 
 ## monotonic spline age-depth model using scam() from pkg scam
 test_that("simulate() works with a scam", {
-    data(smallAges)
-    smallAges$Error[1] <- 1.1
-    sw <- scam(Date ~ s(Depth, k = 5, bs = "mpd"), data = smallAges,
-               weights = 1 / smallAges$Error, gamma = 1.4)
     sims <- simulate(sw, nsim = 5, seed = 42)
     expect_identical(nrow(sims), 12L)
     expect_identical(ncol(sims), 5L)
 })
 
+test_that("simulate() works with no .Random.seed", {
+    rm(".Random.seed", envir = globalenv())
+    sims <- simulate(m1, nsim = 5, seed = 42)
+    expect_identical(nrow(sims), 400L)
+    expect_identical(ncol(sims), 5L)
+
+    rm(".Random.seed", envir = globalenv())
+    sims <- simulate(m2, nsim = 5, seed = 42)
+    expect_identical(nrow(sims), 400L)
+    expect_identical(ncol(sims), 5L)
+
+    rm(".Random.seed", envir = globalenv())
+    sims <- simulate(sw, nsim = 5, seed = 42)
+    expect_identical(nrow(sims), 12L)
+    expect_identical(ncol(sims), 5L)
+})
+
+test_that("simulate() works with out a seed", {
+    sims <- simulate(m1, nsim = 5)
+    expect_identical(nrow(sims), 400L)
+    expect_identical(ncol(sims), 5L)
+
+    sims <- simulate(m2, nsim = 5)
+    expect_identical(nrow(sims), 400L)
+    expect_identical(ncol(sims), 5L)
+
+    sims <- simulate(sw, nsim = 5)
+    expect_identical(nrow(sims), 12L)
+    expect_identical(ncol(sims), 5L)
+})
