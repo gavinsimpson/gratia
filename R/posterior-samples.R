79082d71b62b8e2b8b34fc70992fed9e623fcc59
Gavin Simpson <ucfagls@gmail.com>
2018-11-18 23:00:43 -0600

    add skeleton for smooth_samples; don't dot name the row variable

--- R/posterior-samples.R
+++ R/posterior-samples.R
@@ -17,6 +17,16 @@
 ##'   then defaults to `object[["prior.weights"]]`, otherwise a vector of ones.
 ##' @param ... arguments passed to other methods
 ##'
+##' @return A tibble (data frame) with 3 columns containing the posterior
+##'   predicted values in long format. The columns are
+##' * `row` (integet) the row of `newdata` that each posterior draw relates to,
+##' * `draw` (integer) an index, in range `1:n`, indicating which draw each row
+##'     relates to,
+##' * `response` (numeric) the predicted response for the indicated row of
+##'     `newdata`.
+##'
+##' @author Gavin L. Simpson
+##'
 ##' @export
 `posterior_samples` <- function(model, ...) {
     UseMethod("posterior_samples")
@@ -37,6 +47,20 @@
 }
 
 ##' Draw fitted values from the posterior distribution
+##'
+##' Expectations (fitted values) of the response drawn from the posterior
+##'   distribution of fitted model, created via `simulate()` (e.g.
+##'   [simulate.gam()]) and returned in a tidy, long, format.
+##'
+##' @return A tibble (data frame) with 3 columns containing the posterior
+##'   predicted values in long format. The columns are
+##' * `row` (integet) the row of `newdata` that each posterior draw relates to,
+##' * `draw` (integer) an index, in range `1:n`, indicating which draw each row
+##'     relates to,
+##' * `response` (numeric) the predicted response for the indicated row of
+##'     `newdata`.
+##'
+##' @author Gavin L. Simpson
 ##'
 ##' @inheritParams posterior_samples
 ##' @export
@@ -97,8 +121,8 @@
 
     sims <- as_data_frame(sims)
     names(sims) <- as.character(seq_len(ncol(sims)))
-    sims <- add_column(sims, .row = seq_len(nrow(sims)))
-    sims <- gather(sims, key = "draw", value = "fitted", - .row)
+    sims <- add_column(sims, row = seq_len(nrow(sims)))
+    sims <- gather(sims, key = "draw", value = "fitted", - row)
     sims[["draw"]] <- as.integer(sims[["draw"]])
     attr(sims, "seed") <- RNGstate
     sims
@@ -112,7 +136,7 @@
 ##'
 ##' @return A tibble (data frame) with 3 columns containing the posterior
 ##'   predicted values in long format. The columns are
-##' * `.row` (integet) the row of `newdata` that each posterior draw relates to,
+##' * `row` (integet) the row of `newdata` that each posterior draw relates to,
 ##' * `draw` (integer) an index, in range `1:n`, indicating which draw each row
 ##'     relates to,
 ##' * `response` (numeric) the predicted response for the indicated row of
@@ -152,9 +176,54 @@
     RNGstate <- attr(sims, "seed")
     sims <- as_data_frame(sims)
     names(sims) <- as.character(seq_len(ncol(sims)))
-    sims <- add_column(sims, .row = seq_len(nrow(sims)))
-    sims <- gather(sims, key = "draw", value = "response", - .row)
+    sims <- add_column(sims, row = seq_len(nrow(sims)))
+    sims <- gather(sims, key = "draw", value = "response", - row)
     sims[["draw"]] <- as.integer(sims[["draw"]])
     attr(sims, "seed") <- RNGstate
     sims
+}
+
+##' Draw predicted values from the posterior distribution
+##'
+##' Predicted values of the response drawn from the posterior distribution of
+##'   fitted model, created via `simulate()` (e.g. [simulate.gam()])
+##'   and returned in a tidy, long, format.
+##'
+##' @return A tibble (data frame) with 3 columns containing the posterior
+##'   predicted values in long format. The columns are
+##' * `row` (integet) the row of `newdata` that each posterior draw relates to,
+##' * `draw` (integer) an index, in range `1:n`, indicating which draw each row
+##'     relates to,
+##' * `response` (numeric) the predicted response for the indicated row of
+##'     `newdata`.
+##'
+##' @author Gavin L. Simpson
+##'
+##' @inheritParams posterior_samples
+##' @export
+##'
+##' @examples
+##' library("mgcv")
+##' \dontshow{set.seed(2)}
+##' dat <- gamSim(1, n = 400, dist = "normal", scale = 2)
+##' m1 <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
+##'
+##' ## smooth_samples(m1, term = "s(x0)", n = 5, seed = 42)
+`smooth_samples` <- function(model, ...) {
+    UseMethod("smooth_samples")
+}
+
+##' @export
+`smooth_samples.default` <- function(model, ...) {
+    stop("Don't know how sample from the posterior of <",
+         class(model)[[1L]], ">", .call = FALSE)
+}
+
+##' @export
+##' @rdname smooth_samples
+`smooth_samples.gam` <- function(model, term = NULL, n = 1, newdata = NULL,
+                                 seed = NULL, freq = FALSE, unconditional = FALSE,
+                                 weights = NULL, ...) {
+    S <- smooths(model)            # vector of smooth labels - "s(x)"
+
 }
