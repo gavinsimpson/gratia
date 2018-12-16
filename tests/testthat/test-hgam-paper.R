3513c306513f3d79a66aa56d693789fb04646009
Gavin Simpson <ucfagls@gmail.com>
2018-12-16 15:02:42 -0600

    turn on using 2 threads in some models not run on travis/CRAN

--- tests/testthat/test-hgam-paper.R
+++ tests/testthat/test-hgam-paper.R
@@ -15,6 +15,7 @@ context("test-hgam-paper-models")
 data(CO2, package = "datasets")
 CO2 <- transform(CO2, Plant_uo = factor(Plant, ordered=FALSE))
 data(bird_move, package = "gratia")
+ctrl <- gam.control(nthreads = 2)
 
 ## tests
 ## CO2
@@ -23,7 +24,8 @@ test_that("draw() can plot CO2 model 1", {
     skip_on_travis()
     CO2_mod1 <- gam(log(uptake) ~ s(log(conc), k = 5, bs = "tp") +
                         s(Plant_uo, k = 12, bs = "re"),
-                    data = CO2, method = "REML", family = gaussian())
+                    data = CO2, method = "REML", family = gaussian(),
+                    control = ctrl)
     plt <- draw(CO2_mod1, inc_mean = TRUE)
     expect_doppelganger("hgam-paper-co2-model-1", plt)
 })
@@ -34,7 +36,8 @@ test_that("draw() can plot CO2 model 2", {
     suppressWarnings(
         CO2_mod2 <- gam(log(uptake) ~ s(log(conc), k = 5, m = 2) +
                             s(log(conc), Plant_uo, k = 5,  bs = "fs", m = 2),
-                        data = CO2, method = "REML", family = gaussian())
+                        data = CO2, method = "REML", family = gaussian(),
+                        control = ctrl)
     )
     plt <- draw(CO2_mod2, inc_mean = TRUE)
     expect_doppelganger("hgam-paper-co2-model-2", plt)
@@ -46,7 +49,8 @@ test_that("draw() can plot CO2 model 3", {
     CO2_mod3 <- gam(log(uptake) ~ s(log(conc), k = 5, m = 2, bs = "tp") +
                         s(log(conc), by = Plant_uo, k = 5, m = 1, bs = "tp") +
                         s(Plant_uo, bs = "re", k = 12),
-                    data = CO2, method = "REML")
+                    data = CO2, method = "REML",
+                    control = ctrl)
     plt <- draw(CO2_mod3, inc_mean = TRUE)
     expect_doppelganger("hgam-paper-co2-model-3", plt)
 })
@@ -55,7 +59,8 @@ test_that("draw() can plot CO2 model 4", {
     skip_on_cran()
     skip_on_travis()
     CO2_mod4 <- gam(log(uptake) ~ s(log(conc), Plant_uo, k=5, bs="fs", m=2),
-                    data=CO2, method="REML")
+                    data=CO2, method="REML",
+                    control = ctrl)
     plt <- draw(CO2_mod4, inc_mean = TRUE)
     expect_doppelganger("hgam-paper-co2-model-4", plt)
 })
@@ -65,7 +70,8 @@ test_that("draw() can plot CO2 model 5", {
     skip_on_travis()
     CO2_mod5 <- gam(log(uptake) ~ s(log(conc), by = Plant_uo, k = 5, bs = "tp", m = 2) +
                         s(Plant_uo, bs="re", k=12),
-                    data = CO2, method = "REML")
+                    data = CO2, method = "REML",
+                    control = ctrl)
     plt <- draw(CO2_mod5, inc_mean = TRUE)
     expect_doppelganger("hgam-paper-co2-model-5", plt)
 })
@@ -76,7 +82,8 @@ test_that("draw() can plot bird_move model 1", {
     skip_on_travis()
     bird_mod1 <- gam(count ~ te(week, latitude, bs=c("cc", "tp"), k = c(10, 10)),
                      data = bird_move, method = "REML", family = poisson(),
-                     knots = list(week = c(0, 52)))
+                     knots = list(week = c(0, 52)),
+                     control = ctrl)
     plt <- draw(bird_mod1)
     expect_doppelganger("hgam-paper-bird-move-model-1", plt)
 })
@@ -89,7 +96,8 @@ test_that("draw() can plot bird_move model 2", {
                          t2(week, latitude, species, bs = c("cc", "tp", "re"),
                             k = c(10, 10, 6), m = 2, full = TRUE),
                      data = bird_move, method = "REML", family = poisson(),
-                     knots = list(week = c(0, 52)))
+                     knots = list(week = c(0, 52)),
+                     control = ctrl)
     plt <- draw(bird_mod2)
     expect_doppelganger("hgam-paper-bird-move-model-2", plt)
 })
@@ -103,7 +111,8 @@ test_that("draw() can plot bird_move model 3", {
                          te(week, latitude, by = species, bs = c("cc", "tp"),
                             k = c(10, 10), m = 1),
                      data = bird_move, method = "REML", family = poisson(),
-                     knots = list(week = c(0, 52)))
+                     knots = list(week = c(0, 52)),
+                     control = ctrl)
     plt <- draw(bird_mod3)
     expect_doppelganger("hgam-paper-bird-move-model-3", plt)
 })
@@ -114,7 +123,8 @@ test_that("draw() throws message with bird_move model 4", {
     bird_mod4 <- gam(count ~ t2(week, latitude, species, bs = c("cc", "tp", "re"),
                                 k = c(10, 10, 6), m = c(2, 2, 2)),
                      data = bird_move, method = "REML", family = poisson(),
-                     knots = list(week = c(0, 52)))
+                     knots = list(week = c(0, 52)),
+                     control = ctrl)
     ## There's nothing we can currently do, as
     expect_message(draw(bird_mod4), "Unable to draw any of the model terms.",
                    fixed = FALSE)
@@ -127,7 +137,8 @@ test_that("draw() can plot bird_move model 5", {
                          te(week, latitude, by = species,
                             bs = c("cc", "tp"), k = c(10, 10), m = 2),
                      data = bird_move, method = "REML", family = poisson(),
-                     knots = list(week = c(0, 52)))
+                     knots = list(week = c(0, 52)),
+                     control = ctrl)
     plt <- draw(bird_mod5)
     expect_doppelganger("hgam-paper-bird-move-model-5", plt)
 })
