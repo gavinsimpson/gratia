aa4795d83fb617f1479fd9453941761111215b13
Gavin Simpson <ucfagls@gmail.com>
2019-01-02 18:39:57 -0600

    document data sets better; closes #23

--- R/datasets.R
+++ R/datasets.R
@@ -14,12 +14,33 @@
 ##' * `SedPerCentChange`
 ##'
 ##' @format A data frame with 12 rows and 7 variables.
+##'
+##' @source Simpson, G.L. (Unpublished data).
+##'
 ##' @keywords data
 ##' @name smallAges
 ##' @docType data
 NULL
 
 ##' Simulated bird migration data
+##'
+##' Data generated from a hypothetical study of bird movement along a migration
+##' corridor, sampled throughout the year. This dataset consists of simulated
+##' sample records of numbers of observed locations of 100 tagged individuals
+##' each from six species of bird, at ten locations along a latitudinal gradient,
+##' with one observation taken every four weeks. Counts were simulated randomly
+##' for each species in each location and week by creating a species-specific
+##' migration curve that gave the probability of finding an individual of a
+##' given species in a given location, then simulated the distribution of
+##' individuals across sites using a multinomial distribution, and subsampling
+##' that using a binomial distribution to simulation observation error (i.e.
+##' not every bird present at a location would be detected). The data set
+##' (`bird_move`) consists of the variables `count`, `latitude`, `week` and
+##' `species`.
+##'
+##' @source Pedersen EJ, Miller DL, Simpson GL, Ross N. 2018. Hierarchical
+##'   generalized additive models: an introduction with mgcv. *PeerJ Preprints*
+##'   **6**:e27320v1 [doi: 10.7287/peerj.preprints.27320v1](https://doi.org/10.7287/peerj.preprints.27320v1).
 ##'
 ##' @format A data frame
 ##' @name bird_move
@@ -28,6 +49,28 @@ NULL
 NULL
 
 ##' Madison lakes zooplankton data
+##'
+##' The Madison lake zooplankton data are from a long-term study in seasonal
+##' dynamics of zooplankton, collected by the Richard Lathrop. The data were
+##' collected from a chain of lakes in Wisconsin (Mendota, Monona, Kegnonsa,
+##' and Waubesa) approximately bi-weekly from 1976 to 1994. They consist of
+##' samples of the zooplankton communities, taken from the deepest point of each
+##' lake via vertical tow. The data are provided by the Wisconsin Department of
+##' Natural Resources and their collection and processing are fully described in
+##' Lathrop (2000).
+##'
+##' Each record consists of counts of a given zooplankton taxon taken from a
+##' subsample from a single vertical net tow, which was then scaled to account
+##' for the relative volume of subsample versus the whole net sample and the area
+##' of the net tow and rounded to the nearest 1000 to give estimated population
+##' density per m2 for each taxon at each point in time in each sampled lake.
+##'
+##' @source Pedersen EJ, Miller DL, Simpson GL, Ross N. 2018. Hierarchical
+##'   generalized additive models: an introduction with mgcv. *PeerJ Preprints*
+##'   **6**:e27320v1 [doi: 10.7287/peerj.preprints.27320v1](https://doi.org/10.7287/peerj.preprints.27320v1).
+##'
+##' @references Lathrop RC. (2000). Madison Wisonsin Lakes Zooplankton 1976--1994.
+##'   Environmental Data Initiative.
 ##'
 ##' @format A data frame
 ##' @name zooplankton
