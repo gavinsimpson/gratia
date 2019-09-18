15ede142b480b8469620ca3d99742b03ec6f8a97
Gavin L. Simpson <ucfagls@gmail.com>
2019-09-18 17:19:37 -0600

    add some tests for subsetting functions

---
+++ tests/testthat/test-subsetting.R
@@ -0,0 +1,25 @@
+## Test subsetting methods `[`
+
+## load packages
+library("testthat")
+library("mgcv")
+library("gratia")
+
+context("Testing subsetting methods")
+
+set.seed(12398)
+dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
+m2 <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat, method = "REML")
+
+test_that("subsetting works for smooth_samples", {
+    sm <- smooth_samples(m2, seed = 42)
+    hsm <- head(sm)
+    expect_identical(NROW(hsm), 6L)
+    expect_true(! is.null(attr(hsm, "data_names")))
+    expect_true(! is.null(attr(hsm, "seed")))
+
+    attrs <- attributes(hsm)
+    expect_type(attrs, "list")
+    expect_named(attrs, expected = c("row.names", "names", "class",
+                                     "seed", "data_names"))
+})
