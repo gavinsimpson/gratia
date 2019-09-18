58d127eb0867db19110ffdff02277b03993c803b
Gavin L. Simpson <ucfagls@gmail.com>
2019-09-18 14:28:47 -0600

    subsetting code for smooth_samples

--- R/subsetting.R
+++ R/subsetting.R
@@ -7,3 +7,17 @@
     x
 }
 
+##' @export
+`[.smooth_samples` <- function(x, i, j, drop = FALSE) {
+    cls <- class(x)
+    seed <- attr(x, "seed")
+    data_names <- attr(x, "data_names")
+    class(x) <- class(x)[-c(1:2)]
+    x <- NextMethod()
+    class(x) <- cls
+    take <- unique(x$smooth)
+    data_names <- data_names[take]
+    attr(x, "seed") <- seed
+    attr(x, "data_names") <- data_names
+    x
+}
