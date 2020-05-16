60523b00ebf1c9cfe2e23d2e3d9fea876c64156c
Gavin Simpson <ucfagls@gmail.com>
2020-05-16 16:06:20 -0600

    initial work for a nw evaluate_smooth

--- R/evaluate_smooth2.R
+++ R/evaluate_smooth2.R
@@ -29,10 +29,10 @@
                                                unconditional = unconditional,
                                                overall_uncertainty = overall_uncertainty),
                    `2d_smooth`  = eval_2d_smooth(smooths[[i]],
-                                               n = n,
-                                               newdata = newdata,
-                                               unconditional = unconditional,
-                                               overall_uncertainty = overall_uncertainty),
+                                                 n = n,
+                                                 newdata = newdata,
+                                                 unconditional = unconditional,
+                                                 overall_uncertainty = overall_uncertainty),
                    re_smooth  = eval_re_smooth(smooths[[i]],
                                                newdata = newdata,
                                                unconditional = unconditional),
@@ -72,4 +72,129 @@
     }
 
     sm_type
+}
+
+##' @importFrom tibble
+##' @importFrom rlang := !!
+`check_user_data` <- function(data, vars) {
+    if (is.data.frame(data)) {
+        if (!all(vars %in% names(data))) {
+            stop(paste("Variable", smooth_var, "not found in 'data'."))
+        }
+    } else if (is.numeric(newdata)) {   # vector; coerce to data frame
+        if (length(vars) > 1L) {
+            stop("'smooth' requires multiple data vectors but only 1 provided.")
+        }
+        data <- tibble(!!(vars) := data)
+    } else {                            # object we can't handle; bail out
+        stop("'data', if supplied, must be a numeric vector or a data frame.")
+    }
+    data
+}
+
+## Returns the type of smoother as far as mgcv is concerned
+`mgcv_type` <- function(smooth) {
+    stopifnot(is_mgcv_smooth(smooth))
+    cls <- class(smooth)[1L]
+    cls <- sub("\\.smooth$", "", cls)
+    cls
+}
+
+##' @importFrom tibble tibble add_column
+##' @importFrom rlang := !!
+`spline_values2` <- function(smooth, data, model, unconditional,
+                             overall_uncertainty = TRUE, term) {
+    X <- PredictMat(smooth, data)   # prediction matrix
+    start <- smooth[["first.para"]]
+    end <- smooth[["last.para"]]
+    para.seq <- start:end
+    coefs <- coef(model)[para.seq]
+    fit <- drop(X %*% coefs)
+
+    label <- smooth_label(smooth)
+
+    ## want full vcov for component-wise CI
+    V <- get_vcov(model, unconditional = unconditional)
+
+    ## variables for component-wise CIs for smooths
+    column_means <- model[["cmX"]]
+    lcms <- length(column_means)
+    nc <- ncol(V)
+    meanL1 <- smooth[["meanL1"]]
+
+    if (isTRUE(overall_uncertainty) && attr(smooth, "nCons") > 0L) {
+        if (lcms < nc) {
+            column_means <- c(column_means, rep(0, nc - lcms))
+        }
+        Xcm <- matrix(column_means, nrow = nrow(X), ncol = nc, byrow = TRUE)
+        if (!is.null(meanL1)) {
+            Xcm <- Xcm / meanL1
+        }
+        Xcm[, para.seq] <- X
+        rs <- rowSums((Xcm %*% V) * Xcm)
+    } else {
+        rs <- rowSums((X %*% V[para.seq, para.seq]) * X)
+    }
+
+    se.fit <- sqrt(pmax(0, rs))
+
+    d <- smooth_dim(smooth)
+    sm_var <- smooth_variable(smooth)
+    ## Return
+    out <- if (d == 1L) {
+        if (is_fs_smooth(smooth)) {
+            stop("Not yet implemented.")
+            tibble(smooth = rep(label, nrow(X)),
+                   x = newdata[, 1L], f = newdata[, 2L],
+                   est = fit, se = se.fit)
+        } else {
+            tibble(smooth = rep(label, nrow(X)),
+                   !!(sm_var) := pull(data, sm_var),
+                   est = fit, se = se.fit)
+        }
+    } else {
+        tibble(smooth = rep(label, nrow(X)),
+               !!(sm_var[1L]) := pull(data, sm_var[1L]),
+               !!(sm_var[2L]) := pull(data, sm_var[2L]),
+               est = fit, se = se.fit)
+    }
+
+    nr <- nrow(out)
+    
+    out <- add_column(out, type = rep(mgcv_type(smooth), nr),
+                      .after = 1L)
+    out
+}
+
+##' @importFrom tibble add_column
+`eval_1d_smooth` <- function(smooth, model, n = 100, data = NULL,
+                             unconditional = FALSE,
+                             overall_uncertainty = TRUE) {
+    is_by <- is_by_smooth(smooth)
+    by_var <- by_variable(smooth) # get even if not a by as we want NA later
+
+    sm_var <- smooth_variable(smooth)
+
+    ## deal with data if supplied
+    data <- if (is.null(data)) {
+        smooth_data(model = model, n = n, smooth = smooth)
+    } else {
+        vars <- sm_var
+        if (is.na(by_var)) {
+            vars <- append(vars, by_var)
+        }
+        check_user_data(data, vars)
+    }
+    
+    eval_sm <- spline_values2(smooth, data = data,
+                              unconditional = unconditional,
+                              model = model,
+                              overall_uncertainty = overall_uncertainty,
+                              term = sm_var)
+
+    ## add on info regarding by variable
+    eval_sm <- add_column(eval_sm, by = rep(by_var, nrow(eval_sm)),
+                          .after = 2L)
+
+    eval_sm
 }
