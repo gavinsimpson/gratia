4b192028d44c945f97eb9c9cadc15e463085e310
Gavin Simpson <ucfagls@gmail.com>
2020-05-17 11:13:15 -0600

    test for equality rather identicality

--- tests/testthat/test-data-sim.R
+++ tests/testthat/test-data-sim.R
@@ -11,6 +11,6 @@ data(ref_sims, package = "gratia")
 test_that("test data_sim() reproduces expected output", {
     new_sims <- create_reference_simulations()
     for (i in names(ref_sims)) {
-        expect_identical(new_sims[[!!(i)]], ref_sims[[!!(i)]])
+        expect_equal(new_sims[[!!(i)]], ref_sims[[!!(i)]])
     }
 })
