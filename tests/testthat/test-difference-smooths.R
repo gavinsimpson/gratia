0ce07c6ae5e91e20213bad198166929b7afd211b
Gavin Simpson <ucfagls@gmail.com>
2020-05-29 09:46:31 -0600

    don't run some tests on cran

--- tests/testthat/test-difference-smooths.R
+++ tests/testthat/test-difference-smooths.R
@@ -10,27 +10,31 @@ context("Test difference_smooths()")
 
 set.seed(23)
 df <- data_sim("eg4", n = 100)
-m_gam   <- gam(y ~ fac + s(x2, by = fac) + s(x0), data = df, method = "REML")
-m_bam   <- bam(y ~ fac + s(x2, by = fac) + s(x0), data = df)
-m_gamm  <- gamm(y ~ fac + s(x2, by = fac) + s(x0), data = df)
 m_gamm4 <- gamm4(y ~ fac + s(x2, by = fac) + s(x0), data = df)
 
 test_that("difference_smooths() works for a gam model", {
+    m_gam <- gam(y ~ fac + s(x2, by = fac) + s(x0), data = df, method = "REML")
     expect_silent(ds <- difference_smooths(m_gam, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
 })
 
 test_that("difference_smooths() works for a bam model", {
+    skip_on_cran()
+    m_bam <- bam(y ~ fac + s(x2, by = fac) + s(x0), data = df)
     expect_silent(ds <- difference_smooths(m_bam, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
 })
 
 test_that("difference_smooths() works for a gamm model", {
+    skip_on_cran()
+    m_gamm <- gamm(y ~ fac + s(x2, by = fac) + s(x0), data = df)
     expect_silent(ds <- difference_smooths(m_gamm, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
 })
 
 test_that("difference_smooths() works for a gamm4 model", {
+    skip_on_cran()
+    m_gamm4 <- gamm4(y ~ fac + s(x2, by = fac) + s(x0), data = df)
     expect_silent(ds <- difference_smooths(m_gamm4, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
 })
