98d8be29bc57023940f656530b724dfd66508d59
Gavin Simpson <ucfagls@gmail.com>
2020-05-28 18:01:07 -0600

    don't print an example that was giving different results on MKL

--- R/posterior-samples.R
+++ R/posterior-samples.R
@@ -265,7 +265,8 @@
 ##'
 ##' ## fit model...
 ##' m2 <- gam(y ~ fac + s(x2, by = fac) + s(x0), data = dat)
-##' smooth_samples(m2, n = 5, seed = 42)
+##' sms <- smooth_samples(m2, n = 5, seed = 42)
+##' draw(sms)
 ##' \dontshow{options(op)}
 `smooth_samples` <- function(model, ...) {
     UseMethod("smooth_samples")
