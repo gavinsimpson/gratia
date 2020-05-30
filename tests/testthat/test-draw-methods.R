15c6856f7de86541b6a166e1c95e95a4c199f010
Gavin Simpson <ucfagls@gmail.com>
2020-05-30 12:32:50 -0600

    skip a test on CI

--- tests/testthat/test-draw-methods.R
+++ tests/testthat/test-draw-methods.R
@@ -434,6 +434,8 @@ test_that("draw() can handle a mixture of numeric and factor random effects", {
 
 test_that("draw.gam uses fixed scales if asked for them: #73", {
     skip_on_cran()
+    skip_on_ci()
+    df <- data_sim("eg1", n = 1000, seed = 1)
     m <- gam(y ~ s(x1) + s(x2) + ti(x1, x2), data = dat1, method = "REML")
     plt <- draw(m, scales = "fixed")
     vdiffr:::expect_doppelganger("issue 73 draw uses fixed scales if asked for them",
