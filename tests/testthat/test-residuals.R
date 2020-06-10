a3e9a3ea7093b2829549986d9c20d1851245f719
Gavin Simpson <ucfagls@gmail.com>
2020-06-10 16:47:06 -0600

    add tests for partial_residuals() methods for bam(), gamm(), and gamm4() fits

--- tests/testthat/test-residuals.R
+++ tests/testthat/test-residuals.R
@@ -4,16 +4,41 @@
 library("testthat")
 library("gratia")
 library("mgcv")
+library("gamm4")
 
 context("Test partial_residuals")
 
-N <- 100L
-data <- data_sim("eg1", n = N, seed = 42)
+N <- 400L
+df <- data_sim("eg1", n = N, seed = 42)
 ## fit the model
-m <-  gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = data, method = 'REML')
+m       <-  gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = df, method = 'REML')
+m_bam   <-  bam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = df, method = 'fREML')
+m_gamm  <-  gamm(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = df)
+m_gamm4 <-  gamm4(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = df)
 
 test_that("partial_residuals returns a tibble", {
     expect_silent(p_res <- partial_residuals(m))
+    expect_s3_class(p_res, class = c("tbl_df", "tbl", "data.frame"), exact = TRUE)
+    expect_named(p_res, c("s(x0)", "s(x1)", "s(x2)", "s(x3)"))
+    expect_identical(nrow(p_res), N)
+})
+
+test_that("partial_residuals returns a tibble", {
+    expect_silent(p_res <- partial_residuals(m_bam))
+    expect_s3_class(p_res, class = c("tbl_df", "tbl", "data.frame"), exact = TRUE)
+    expect_named(p_res, c("s(x0)", "s(x1)", "s(x2)", "s(x3)"))
+    expect_identical(nrow(p_res), N)
+})
+
+test_that("partial_residuals returns a tibble", {
+    expect_silent(p_res <- partial_residuals(m_gamm))
+    expect_s3_class(p_res, class = c("tbl_df", "tbl", "data.frame"), exact = TRUE)
+    expect_named(p_res, c("s(x0)", "s(x1)", "s(x2)", "s(x3)"))
+    expect_identical(nrow(p_res), N)
+})
+
+test_that("partial_residuals returns a tibble", {
+    expect_silent(p_res <- partial_residuals(m_gamm4))
     expect_s3_class(p_res, class = c("tbl_df", "tbl", "data.frame"), exact = TRUE)
     expect_named(p_res, c("s(x0)", "s(x1)", "s(x2)", "s(x3)"))
     expect_identical(nrow(p_res), N)
