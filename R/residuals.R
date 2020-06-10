2dd69d1ce975f3dd8a977c2223880396907d46eb
Gavin Simpson <ucfagls@gmail.com>
2020-06-10 12:09:38 -0600

    implements #76

---
+++ R/residuals.R
@@ -0,0 +1,55 @@
+##' Partial residuals
+##'
+##' @param object an R object, typically a model. Currently only objects of
+##'   class `"gam"` (or that inherit from that class) are supported.
+##' @param ... arguments passed to other methods.
+##'
+##' @export
+`partial_residuals` <- function(object, ...) {
+    UseMethod("partial_residuals")
+}
+
+##' @param select character, logical, or numeric; which smooths to plot. If
+##'   `NULL`, the default, then all model smooths are drawn. Numeric `select`
+##'   indexes the smooths in the order they are specified in the formula and
+##'   stored in `object`. Character `select` matches the labels for smooths
+##'   as shown for example in the output from `summary(object)`. Logical
+##'   `select` operates as per numeric `select` in the order that smooths are
+##'   stored.
+##' @param partial_match logical; should smooths be selected by partial matches
+##'   with `select`? If `TRUE`, `select` can only be a single string to match
+##'   against.
+##'
+##' @rdname partial_residuals
+##'
+##' @export
+##' 
+##' @importFrom tibble as_tibble
+##' @importFrom dplyr bind_cols arrange
+`partial_residuals.gam` <- function(object, select = NULL, partial_match = FALSE,
+                                    ...) {
+    ## get a vector of labels for smooths
+    sms <- smooths(object)
+    ## which were selected; select = NULL -> all selected
+    take <- check_user_select_smooths(sms, select = select,
+                                      partial_match = partial_match)
+    if (!any(take)) {
+        stop("No smooth label matched 'select'. Try with 'partial_match = TRUE'?",
+             call. = FALSE)
+    }
+    sms <- sms[take] # subset to selected smooths
+
+    ## get the contributions for each selected smooth
+    p_terms <- predict(object, type = "terms", terms = sms)
+    attr(p_terms, "constant") <- NULL # remove intercept attribute
+    ## weight residuals...
+    w_resid <- object$residuals * sqrt(object$weights)
+    ## and compute partial residuals
+    p_resids <- p_terms + w_resid
+
+    ## cast as a tibble --- do something with the column names?
+    ##  - they are non-standard: `s(x)` for example
+    p_resids <- tibble::as_tibble(p_resids)
+
+    p_resids
+}
