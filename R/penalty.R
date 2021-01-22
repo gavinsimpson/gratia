7aee4e6c21eaaf236a3c399245418e3a48807826
Gavin Simpson <ucfagls@gmail.com>
2021-01-21 19:29:56 -0600

    revamp penalty to include more info

--- R/penalty.R
+++ R/penalty.R
@@ -3,6 +3,9 @@
 ##' @param object a fitted GAM or a smooth.
 ##' @param smooth character; vector of smooths to extract penalty matrices for.
 ##'   If `NULL`, penalty matrices for all smooths in `object` are extracted.
+##' @param rescale logical; by default, *mgcv* will scale the penalty matrix for
+##'   better performance in [mgcv::gamm()]. If `rescale` is `TRUE`, this scaling
+##'   will be undone to put the penalty matrix back on the original scale.
 ##' @param margins logical; extract the penalty matrices for the tensor
 ##'   product or the margainl smooths of the tensor product?
 ##' @param ... additional arguments passed to methods.
@@ -25,7 +28,7 @@
 
 ##' @export
 ##' @rdname penalty
-`penalty.gam` <- function(object, smooth = NULL, ...) {
+`penalty.gam` <- function(object, smooth = NULL, rescale = FALSE, ...) {
     ## are particular smooths selected
     smooth_ids <- if (!is.null(smooth)) {
          which_smooths(object, smooth) # which smooths match 'smooth'
@@ -37,7 +40,7 @@
     smooths <- get_smooths_by_id(object, smooth_ids)
 
     ## loop over the smooths applying penalty to each
-    pen <- lapply(smooths, penalty)
+    pen <- lapply(smooths, penalty, rescale = rescale)
     pen <- bind_rows(pen)
     class(pen) <- c("tidy_penalty", class(pen))
     pen
@@ -45,19 +48,37 @@
 
 ##' @export
 ##' @rdname penalty
-`penalty.mgcv.smooth` <- function(object, ...) {
+`penalty.mgcv.smooth` <- function(object, rescale = FALSE, ...) {
+    ## smooth label
+    sm_lab <- smooth_label(object)
+    ## type of smooth
+    sm_type <- smooth_type(object)
     ## extract the set of penalty matrices
     S <- object[["S"]] # S is a list even if length(S) == 1
     sp_label <- names(object[["sp"]]) # penalty matrix label
     pen <- vector("list", length = length(S))
     ## loop over penalty matrices & tidy each of them
-    for (i in seq_along(pen)) {
-        pen[[i]] <- tidy_penalty(S[[i]], sp_label[i])
+    pen_seq <- seq_along(pen)
+    for (i in pen_seq) {
+        pen[[i]] <- tidy_penalty(S[[i]], smooth = sm_lab,
+                                 type = sm_type,
+                                 label = sp_label[i])
+    }
+
+    ## should the default penalty rescaling for gamm performance be reversed?
+    if (rescale) {
+        for (i in pen_seq) {
+            S_scale <- object[["S.scale"]][i]
+            if (is.null(S_scale)) {
+                S_scale <- 1
+            }
+            pen[[i]][["value"]] <- pen[[i]][["value"]] * S_scale
+        }
     }
 
     ## combine the tidy penalty matrices into a single tibble
     pen <- bind_rows(pen)
-    class(pen) <- c("tidy_penalty", class(pen))
+    class(pen) <- c("penalty_df", class(pen))
     pen
 }
 
@@ -76,13 +97,17 @@
 ##' @importFrom tibble add_column as_tibble
 ##' @importFrom tidyr pivot_longer
 ##' @importFrom dplyr starts_with
-`tidy_penalty` <- function(s, label) {
+`tidy_penalty` <- function(s, smooth, type, label) {
     rownames(s) <- paste0("f", seq_len(nrow(s)))
     colnames(s) <- paste0("f", seq_len(ncol(s)))
     s <- as_tibble(s)
-    s <- add_column(s, r = paste0("f", seq_len(nrow(s))), .before = 1L)
-    s <- pivot_longer(s, cols = starts_with("f"), names_to = "c",
-                        values_to = "value")
-    s <- add_column(s, sp = rep(label, nrow(s)), .before = 1L)
+    s <- add_column(s, row = paste0("f", seq_len(nrow(s))), .before = 1L)
+    s <- pivot_longer(s, cols = starts_with("f"), names_to = "col",
+                      values_to = "value")
+    ns <- nrow(s)
+    s <- add_column(s,
+                    smooth  = rep(smooth, ns),
+                    type    = rep(type, ns),
+                    penalty = rep(label, ns), .before = 1L)
     s
 }
