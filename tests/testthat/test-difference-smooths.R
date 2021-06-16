2c5c0f1c49c0cdc25682dc714b51555ee8dd9351
Gavin Simpson <ucfagls@gmail.com>
2021-06-16 13:01:00 +0200

    add tests of draw.difference_smooths

--- tests/testthat/test-difference-smooths.R
+++ tests/testthat/test-difference-smooths.R
@@ -8,14 +8,23 @@ library("gamm4")
 
 context("Test difference_smooths()")
 
-set.seed(23)
-df <- data_sim("eg4", n = 100)
-m_gamm4 <- gamm4(y ~ fac + s(x2, by = fac) + s(x0), data = df)
+## Need a local wrapper to allow conditional use of vdiffr
+`expect_doppelganger` <- function(title, fig, path = NULL, ...) {
+  testthat::skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(title, fig, path = path, ...)
+}
+
+## generate data
+df <- data_sim("eg4", n = 100, seed = 23)
 
 test_that("difference_smooths() works for a gam model", {
     m_gam <- gam(y ~ fac + s(x2, by = fac) + s(x0), data = df, method = "REML")
     expect_silent(ds <- difference_smooths(m_gam, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
+
+    ## plot
+    plt <- draw(ds)
+    expect_doppelganger("draw difference_smooths gam", plt)
 })
 
 test_that("difference_smooths() works for a bam model", {
@@ -23,6 +32,10 @@ test_that("difference_smooths() works for a bam model", {
     m_bam <- bam(y ~ fac + s(x2, by = fac) + s(x0), data = df)
     expect_silent(ds <- difference_smooths(m_bam, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
+
+    ## plot
+    plt <- draw(ds)
+    expect_doppelganger("draw difference_smooths bam", plt)
 })
 
 test_that("difference_smooths() works for a gamm model", {
@@ -30,6 +43,10 @@ test_that("difference_smooths() works for a gamm model", {
     m_gamm <- gamm(y ~ fac + s(x2, by = fac) + s(x0), data = df)
     expect_silent(ds <- difference_smooths(m_gamm, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
+
+    ## plot
+    plt <- draw(ds)
+    expect_doppelganger("draw difference_smooths gamm", plt)
 })
 
 test_that("difference_smooths() works for a gamm4 model", {
@@ -37,4 +54,8 @@ test_that("difference_smooths() works for a gamm4 model", {
     m_gamm4 <- gamm4(y ~ fac + s(x2, by = fac) + s(x0), data = df)
     expect_silent(ds <- difference_smooths(m_gamm4, smooth = "s(x2)"))
     expect_s3_class(ds, c("difference_smooth", "tbl_df", "tbl", "data.frame"))
+
+    ## plot
+    plt <- draw(ds)
+    expect_doppelganger("draw difference_smooths gamm4", plt)
 })
