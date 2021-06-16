de7f1db4a504e600f6d4a3b2947318726ce176d7
Gavin Simpson <ucfagls@gmail.com>
2021-06-16 12:06:19 +0200

    improvements to edf to extract the smoothness parameter uncertainty corrected version

--- R/edf.R
+++ R/edf.R
@@ -2,8 +2,12 @@
 ##'
 ##' Extracts the effective degrees of freedom (EDF) for model smooth terms
 ##'
-##' @details Wood (2017; pp. 252) describes an alternative EDF describes an
-##'   alternative EDF form for the entire model
+##' @details Multiple formulations for the effective degrees of freedom are
+##'   available. The additional uncertainty due to selection of smoothness
+##'   parameters can be taken into account when computing the EDF of smooths.
+##'   This form of the EDF is available with `type = "unconditional"`.
+##'
+##'   Wood (2017; pp. 252) describes an alternative EDF for the  model
 ##'   \deqn{\mathrm{EDF} = 2\mathrm{tr}(\mathbf{F}) -
 ##'   \mathrm{tr}(\mathbf{FF}),}{EDF = 2 * tr(F) - tr(F),} where
 ##'   \eqn{\mathrm{tr}} is the matrix trace and \eqn{\mathbf{F}}{F} is a matrix
@@ -23,21 +27,29 @@
 ##' df <- data_sim("eg1", n = 400, seed = 42)
 ##' m <- gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = df, method = "REML")
 ##' edf(m)
+##' ## selected smooths
 ##' edf(m, smooth = c("s(x0)", "s(x2)"))
+##' ## accounting for smoothness parameter uncertainty
+##' edf(m, type = "unconditional")
 `edf` <- function(object, ...) {
     UseMethod("edf")
 }
 
 ##' @param smooth character; a vector of smooth terms whose EDFs will be
 ##'   extracted. If `NULL`, the default, EDFs for all smooths will be returned.
-##' @param alternative logical; return the alternative form for model EDFs of
-##'   Wood (2017; pp. 252).
-##' 
+##' @param type character: which type of EDF to return. `"default"` returns the
+##'   standard EDF; `"unconditional"` selects the EDF corrected for smoothness
+##'   parameter selection, if available; `"alternative"` returns the alternative
+##'   formulation for EDF from Wood (2017, pp. 252)
+##'
 ##' @export
 ##' @importFrom tibble tibble
 ##' @rdname edf
-`edf.gam` <- function(object, smooth = NULL, alternative = FALSE,
+`edf.gam` <- function(object, smooth = NULL,
+                      type = c("default", "unconditional", "alternative"),
                       ...) {
+    ## which type of EDF?
+    type <- match.arg(type)
     ## if particular smooths selected
     sm_ids <- if (!is.null(smooth)) {
         which_smooths(object, smooth) # which smooths match 'smooth'
@@ -52,12 +64,18 @@
     ##     2ed pp 252: 2*trace(F) - trace(F%*%F)
     ##     This doesn't seem to be used at all in `summary.gam()` so not
     ##     sure why Simon extracts it
-    take <- if(isTRUE(alternative)) {
-        "edf1"
-    } else {
-        "edf"
-    }
+    ## - object$edf2 is an EDF that acocunts for smoothness parameter
+    ##     uncertainty; only available for REML and ML fits
+    take <- switch(type,
+                   default = "edf",
+                   unconditional = "edf2",
+                   alternative = "edf1")
     edf_vec <- object[[take]]
+    ## if edf2 is NULL, revert to edf
+    if (is.null(edf_vec)) {
+        edf_vec <- object[["edf"]]
+        warning("Smoothness parameter uncertainty unavailable; using `type = \"default\"`")
+    }
     edf_out <- numeric(length = n_sm)
     sm_labs <- smooths(object)[sm_ids]
     for (i in seq_along(edf_out)) {
