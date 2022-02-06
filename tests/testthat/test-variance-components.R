caffc36417fd4767e671401d6ca5798e9ef12dbe
gavinsimpson <ucfagls@gmail.com>
2022-02-06 20:38:57 +0000

    Deploying to gh-pages from @ gavinsimpson/gratia@218e85facafc38cf44f1919b6655a11c407cd8a5 🚀

---
+++ tests/testthat/test-variance-components.R
@@ -0,0 +1,19 @@
+# Test variance_components()
+
+## load packages
+library("testthat")
+library("gratia")
+library("mgcv")
+library("gamm4")
+
+test_that("variance_comp works for a gam", {
+    expect_silent(vc <- variance_comp(m_gam))
+    expect_s3_class(df, c("variance_comp", "tbl_df", "tbl", "data.frame"))
+    expect_identical(ncol(vc), 5L)
+    expect_identical(nrow(vc), 5L)
+
+    expect_silent(vc <- variance_comp(m_1_smooth))
+    expect_s3_class(df, c("variance_comp", "tbl_df", "tbl", "data.frame"))
+    expect_identical(ncol(vc), 5L)
+    expect_identical(nrow(vc), 2L)
+})
