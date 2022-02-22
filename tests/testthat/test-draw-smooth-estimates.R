6b2c53094715699ad85ea19e0ff65ebf4ddbb7b7
Gavin Simpson <ucfagls@gmail.com>
2022-02-22 16:10:47 +0100

    skip some plotting doppelgangers on mac and windows

--- tests/testthat/test-draw-smooth-estimates.R
+++ tests/testthat/test-draw-smooth-estimates.R
@@ -66,36 +66,48 @@ test_that("draw.smooth_estimates works for su_m_bivar_t2", {
 })
 
 test_that("draw.smooth_estimates works for su_m_trivar", {
+    skip_on_os(os = "win")
+    skip_on_os(os = "mac")
     expect_silent(plt <- draw(smooth_estimates(su_m_trivar, dist = 0.1,
                                                n = 25, n_3d = 4)))
     expect_doppelganger("draw.smooth_estimates su_m_trivar", plt)
 })
 
 test_that("draw.smooth_estimates works for su_m_trivar_te", {
+    skip_on_os(os = "win")
+    skip_on_os(os = "mac")
     expect_silent(plt <- draw(smooth_estimates(su_m_trivar_te, dist = 0.1,
                                                n = 25, n_3d = 4)))
     expect_doppelganger("draw.smooth_estimates su_m_trivar_te", plt)
 })
 
 test_that("draw.smooth_estimates works for su_m_trivar_t2", {
+    skip_on_os(os = "win")
+    skip_on_os(os = "mac")
     expect_silent(plt <- draw(smooth_estimates(su_m_trivar_t2, dist = 0.1,
                                                n = 25, n_3d = 4)))
     expect_doppelganger("draw.smooth_estimates su_m_trivar_t2", plt)
 })
 
 test_that("draw.smooth_estimates works for su_m_quadvar", {
+    skip_on_os(os = "win")
+    skip_on_os(os = "mac")
     expect_silent(plt <- draw(smooth_estimates(su_m_quadvar, dist = 0.1,
                                                n = 25, n_3d = 4, n_4d = 3)))
     expect_doppelganger("draw.smooth_estimates su_m_quadvar", plt)
 })
 
 test_that("draw.smooth_estimates works for su_m_quadvar_te", {
+    skip_on_os(os = "win")
+    skip_on_os(os = "mac")
     expect_silent(plt <- draw(smooth_estimates(su_m_quadvar_te, dist = 0.1,
                                                n = 25, n_3d = 4, n_4d = 3)))
     expect_doppelganger("draw.smooth_estimates su_m_quadvar_te", plt)
 })
 
 test_that("draw.smooth_estimates works for su_m_quadvar_t2", {
+    skip_on_os(os = "win")
+    skip_on_os(os = "mac")
     expect_silent(plt <- draw(smooth_estimates(su_m_quadvar_t2, dist = 0.1,
                                                n = 25, n_3d = 4, n_4d = 3)))
     expect_doppelganger("draw.smooth_estimates su_m_quadvar_t2", plt)
