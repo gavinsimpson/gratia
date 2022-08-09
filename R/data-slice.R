bb7988ace5500a914d7ccbfc8b4bc2b129605151
Gavin Simpson <ucfagls@gmail.com>
2022-08-09 11:48:19 -0400

    clean up

--- R/data-slice.R
+++ R/data-slice.R
@@ -53,7 +53,7 @@
     if (is.null(data)) {
         data <- delete_response(object) %>% as_tibble()
     }
-    
+
     # deal with ...
     ##ellipsis::check_dots_unnamed()
     exprs <- rlang::enquos(...)
@@ -96,84 +96,6 @@
 }
 
 
-#' Deprecated version of `data_slice`
-#'
-#' @param object A model object.
-#' @param var1 character;
-#' @param var2 character;
-#' @param var3 character; ignored currently.
-#' @param var4 character; ignored currently.
-#' @param data a 1-row data frame or tibble containing values for variables in
-#'   the fitted model that are not varying in the slice.
-#' @param n numeric; the number of values to create for each of `var1` and
-#'   `var2`in the slice.
-#' @param offset numeric; value to use for an offset term in the model.
-#' @param ... arguments passed to other methods.
-#'
-#' @export
-#' @rdname old_data_slice
-#'
-#' @importFrom tidyr nesting
-#' @importFrom tibble as_tibble
-#' @importFrom stats model.frame
-#' @importFrom rlang exec
-`old_data_slice.gam` <- function(object, var1, var2 = NULL, var3 = NULL,
-                                 var4 = NULL,
-                                 data = NULL, n = 50, offset = NULL, ...) {
-    ## we need the model frame to get data from
-    mf <- model.frame(object)
-    ## remove response
-    respvar <- attr(object$terms, "response")
-    if (!identical(respvar, 0)) {
-        mf <- mf[, -respvar, drop = FALSE]
-    }
-
-    ## remove offset() var; model.frame returns both `offset(foo(var))` and
-    ## `var`, so we can just remove the former, but we also want to set the
-    ## offset variable `var` to something constant. FIXME
-    if (is.null(offset)) {
-        offset <- 1L
-    }
-    mf <- fix_offset(object, mf, offset_val = offset)
-
-    ## process the variables we focus on for the slice
-    data1 <- process_slice_var(x = var1, data = mf, n = n)
-    ## allow var2 to be null so we can have a 1-d slice
-    data2 <- NULL # needs to be NULL so can be excluded later
-    if (!is.null(var2)) {
-        data2 <- process_slice_var(x = var2, data = mf, n = n)
-    }
-
-    ## if no data, set to NULL; if data supplied, check it:
-    ##   - single row df or list of length-1 elements; only variables in mf
-    data <- process_slice_data(data)
-
-    ## for all other variables in mf not yet supplied, we want the observation
-    ##   from mf that is closest to the median
-    terms_given <- c(var1, var2, var3, var4, names(data))
-    ind <- names(mf) %in% terms_given
-    other_data <- NULL
-    if (any(!ind)) {
-        other_data <- as_tibble(lapply(mf[!ind], value_closest_to_median))
-    }
-
-    ## put all the data objects into a list --- needed for exec() below
-    ## we name the first two elements as otherwise expand_grid() complains
-    ## the other two elements don't want names as the columns would pick them
-    ## up in the expand_grid call later
-    ll <- list(var1 = data1, var2 = data2, data, other_data)
-    ## keep only the non-null elements
-    take <- !vapply(ll, is.null, logical(1))
-    ## expand.grid-alike
-    result <- exec(expand_grid, !!!ll[take])
-    ## fix up the names
-    names(result)[1] <- var1
-    if (!is.null(var2)) {
-        names(result)[2] <- var2
-    }
-
-    result # return
-}
 
 #' @importFrom stats median quantile
 `value_closest_to_median` <- function(x) {
