de1905931ce49428526c7a984b20ae3ee65eedc8
Gavin Simpson <ucfagls@gmail.com>
2022-12-08 14:44:46 +0100

    add a test for #199

---
+++ tests/testthat/test-partial-derivatives.R
@@ -0,0 +1,11 @@
+# uses su_eg2 & su_m_bivar_te
+## load packages
+library("testthat")
+
+# partial derivatives
+test_that("partial derivatives works with single data point", {
+    # data slice through te(x,z) holding z == 0.4
+    ds <- data_slice(su_m_bivar_te, x = 0.0204313075, z = 0.4)
+    expect_silent(pd_x <- partial_derivatives(su_m_bivar_te, data = ds,
+        type = "central", focal = "x"))
+})
