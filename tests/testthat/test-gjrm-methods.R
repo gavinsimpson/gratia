05c86ef67741454bf64587305c07fdd96fa4eea5
gavinsimpson <ucfagls@gmail.com>
2023-01-28 11:39:44 +0000

    Deploying to gh-pages from @ gavinsimpson/gratia@477733b38bd96e89ca40b601dc3a16d72f864f40 🚀

---
+++ tests/testthat/test-gjrm-methods.R
@@ -0,0 +1,31 @@
+## Test confint() methods
+
+## load packages
+library("testthat")
+library("gratia")
+library("mgcv")
+
+# Fit an example GJRM model using the example from ?GJRM::gamlss
+
+test_that("draw works for a simple GJRM gamlss", {
+    skip_if_not_installed("GJRM", minimum_version = "0.2-6")
+    skip_on_cran()
+
+    # follow example from ?GJRM::gamlss
+    library("GJRM")
+    set.seed(0)
+    n <- 400
+    x1 <- round(runif(n))
+    x2 <- runif(n)
+    x3 <- runif(n)
+    f1 <- function(x) cos(pi * 2 * x) + sin(pi * x)
+    y1 <- -1.55 + 2 * x1 + f1(x2) + rnorm(n)
+    dataSim <- data.frame(y1, x1, x2, x3)
+    eq.mu <- y1 ~ x1 + s(x2) + s(x3)
+    eq.s  <-    ~ s(x3)
+    fl    <- list(eq.mu, eq.s)
+    m_gamlss_gjrm <- GJRM::gamlss(fl, data = dataSim)
+    expect_silent(plt <- draw(m_gamlss_gjrm))
+
+    expect_doppelganger("draw-simple-gjrm-gamlss", plt)
+})
