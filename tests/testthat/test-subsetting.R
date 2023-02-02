067aef937ad8fa073ce12267abc568da44d1564b
Gavin Simpson <ucfagls@gmail.com>
2023-02-02 12:48:34 +0100

    smooth_samples no longer needs attribute data_names following #126

--- tests/testthat/test-subsetting.R
+++ tests/testthat/test-subsetting.R
@@ -13,14 +13,10 @@ test_that("subsetting works for smooth_samples", {
     sm <- smooth_samples(m2, seed = 42)
     hsm <- head(sm)
     expect_identical(NROW(hsm), 6L)
-    expect_true(! is.null(attr(hsm, "data_names")))
     expect_true(! is.null(attr(hsm, "seed")))
 
     attrs <- attributes(hsm)
     expect_type(attrs, "list")
     skip_on_cran() # temporary until tibble 3.0 hits CRAN
-    skip_on_travis()
-    skip_on_appveyor()
-    expect_named(attrs, expected = c("names", "row.names", "class",
-                                     "seed", "data_names"))
+    expect_named(attrs, expected = c("names", "row.names", "class", "seed"))
 })
