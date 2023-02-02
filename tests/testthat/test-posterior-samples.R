85a6a1c0a6da2ee552c012cf02eb4c5dafae55f5
Gavin Simpson <ucfagls@gmail.com>
2023-02-02 12:48:09 +0100

    tweak tests to accmodate changes for #126

--- tests/testthat/test-posterior-samples.R
+++ tests/testthat/test-posterior-samples.R
@@ -31,7 +31,7 @@ test_that("smooth_samples works for a multi-smooth GAM", {
                           "tbl", "data.frame"))
     ## 2000 == 4 smooths * 5 * 100
     expect_identical(NROW(sm), 2000L)
-    expect_identical(NCOL(sm), 10L) # 11 cols, 4 univatiate smooths
+    expect_identical(NCOL(sm), 11L) # 11 cols, 4 univatiate smooths
 })
 
 test_that("smooth_samples works for a multi-smooth factor by GAM", {
@@ -41,7 +41,7 @@ test_that("smooth_samples works for a multi-smooth factor by GAM", {
                           "tbl", "data.frame"))
     ## 2000 == 1 + (1 * 3) smooths * 5 * 50
     expect_identical(NROW(sm), 1000L)
-    expect_identical(NCOL(sm), 9L) # 9 cols, univatiate smooths with factor
+    expect_identical(NCOL(sm), 10L) # 10 cols, univatiate smooths with factor
 })
 
 test_that("smooth_samples() fails if not suitable method available", {
