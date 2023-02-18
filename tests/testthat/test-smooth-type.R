c5c52e7e18e2aa23f2b7aa5dbf1c04cc2cf928c9
Gavin Simpson <ucfagls@gmail.com>
2023-02-18 12:03:05 +0100

    add tests for smooth_type()

---
+++ tests/testthat/test-smooth-type.R
@@ -0,0 +1,70 @@
+# Test smooth_type
+
+test_that("smooth_type default method throws error", {
+    expect_error(smooth_type(1), "Unknown type of smooth")
+})
+
+test_that("smooth_type works for TPRS smooths", {
+    expect_silent(st <- smooth_type(get_smooth(m_gam, "s(x0)")))
+    expect_identical(st, "TPRS")
+})
+
+test_that("smooth_type works for CRS smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_penalty, "s(x0)")))
+    expect_identical(st, "CRS")
+})
+
+test_that("smooth_type works for B spline smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_penalty, "s(x1)")))
+    expect_identical(st, "B spline")
+})
+
+test_that("smooth_type works for P spline smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_penalty, "s(x3)")))
+    expect_identical(st, "P spline")
+})
+
+test_that("smooth_type works for CRS shrinkage smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_quick_eg1_shrink, "s(x1)")))
+    expect_identical(st, "CRS (shrink)")
+})
+
+test_that("smooth_type works for TPRS shrinkage smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_quick_eg1_shrink, "s(x0)")))
+    expect_identical(st, "TPRS (shrink)")
+})
+
+test_that("smooth_type works for TPRS shrinkage smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_quick_eg1_shrink, "s(x0)")))
+    expect_identical(st, "TPRS (shrink)")
+})
+
+test_that("smooth_type works for te smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_bivar_te, "te(x,z)")))
+    expect_identical(st, "Tensor product")
+})
+
+test_that("smooth_type works for ti smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_bivar_ti, "ti(x,z)")))
+    expect_identical(st, "Tensor product int.")
+})
+
+test_that("smooth_type works for t2 smooths", {
+    expect_silent(st <- smooth_type(get_smooth(su_m_bivar_t2, "t2(x,z)")))
+    expect_identical(st, "Tensor product (T2)")
+})
+
+test_that("smooth_type works for sz smooths", {
+    expect_silent(st <- smooth_type(get_smooth(m_sz, "s(fac,x2)")))
+    expect_identical(st, "Constr. factor smooth")
+})
+
+test_that("smooth_type works for re smooths", {
+    expect_silent(st <- smooth_type(get_smooth(rm1, "s(fac)")))
+    expect_identical(st, "Random effect")
+})
+
+test_that("smooth_type works for sos smooths", {
+    expect_silent(st <- smooth_type(get_smooth(m_sos, "s(latitude,longitude)")))
+    expect_identical(st, "SOS")
+})
