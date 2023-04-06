d5bbdcde3ac5f10f5bb9431609e9900cb2eecdf2
Gavin Simpson <ucfagls@gmail.com>
2023-04-06 13:56:44 +0200

    fix warning about incorrect usage in tidyselect; add tests for response derivatives

---
+++ tests/testthat/test-response-derivatives.R
@@ -0,0 +1,25 @@
+# uses df_pois & m_nb
+
+# response derivatives
+test_that("response derivatives works", {
+    skip_on_cran()
+    N <- 50L
+    expect_silent(ds <- data_slice(m_nb, x2 = evenly(x2, n = N)))
+    expect_silent(yd <- response_derivatives(m_nb, data = ds,
+        type = "central", focal = "x2"))
+    expect_s3_class(yd, class = "response_derivatives")
+    expect_identical(nrow(yd), N)
+    expect_snapshot(print(yd))
+
+    expect_silent(yd <- response_derivatives(m_nb, data = ds,
+        type = "forward", focal = "x2"))
+    expect_s3_class(yd, class = "response_derivatives")
+    expect_identical(nrow(yd), N)
+    expect_snapshot(print(yd))
+
+    expect_silent(yd <- response_derivatives(m_nb, data = ds,
+        type = "backward", focal = "x2"))
+    expect_s3_class(yd, class = "response_derivatives")
+    expect_identical(nrow(yd), N)
+    expect_snapshot(print(yd))
+})
