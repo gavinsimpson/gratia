156b1c428ef71cd2146ec2abd7ac6d65e688ea32
Gavin Simpson <ucfagls@gmail.com>
2023-10-27 16:25:44 +0200

    add get_smooths_by_id method for glmmTMB models

--- R/glmmtmb-methods.R
+++ R/glmmtmb-methods.R
@@ -23,12 +23,30 @@
 }
 
 # get_smooths_by_id()
+#' @export
+#' @rdname get_smooths_by_id
+`get_smooths_by_id.glmmTMB` <- function(object, id) {
+    if (missing(id)) {
+        stop("`id` of smooth to extract must be provided.", call. = FALSE)
+    }
+    ns <- n_smooths(object)
+    if (ns < 1L) {
+        stop("Model ", deparse(substitute(object)), " contains no smooths.")
+    }
+    if (id > ns) {
+        stop("Attempting to extract a smooth that doesn't exist.", call. = FALSE)
+    }
+    sm <- object$modelInfo$reTrms$cond$smooth_info[[id]][[1L]]
+    stop_if_not_mgcv_smooth(sm)
 
+    sm
+}
 
 # smooth_estimates()
 #' @export
 #' @rdname smooth_estimates
 `smooth_estimates.glmmTMB` <- function(object, ...) {
+    # wrong, but using this to see where things fail
     smooth_estimates.gam(object, ...)
 }
 
