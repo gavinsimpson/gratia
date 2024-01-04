8d2f55896c26866118b06cc0c01349bae1db8f32
Gavin Simpson <ucfagls@gmail.com>
2024-01-04 11:56:07 +0100

    update tests

--- tests/testthat/test-basis.R
+++ tests/testthat/test-basis.R
@@ -1,16 +1,17 @@
 ## Test basis() and related functions
 
+bs_nms <- c(".smooth", ".type", ".by", ".bf", ".value")
+
 test_that("basis() works with a simple smooth", {
     expect_silent(bs <- basis(s(x0), data = su_eg4))
     expect_s3_class(bs, "basis")
-    expect_named(bs, c("smooth", "type", "by_variable", "bf", "value", "x0"))
+    expect_named(bs, c(bs_nms, "x0"))
 })
 
 test_that("basis() works with a factor by smooth", {
     expect_silent(bs <- basis(s(x2, by = fac), data = su_eg4))
     expect_s3_class(bs, "basis")
-    expect_named(bs, c("smooth", "type", "by_variable", "bf", "value", "x2",
-        "fac"))
+    expect_named(bs, c(bs_nms, "x2", "fac"))
 })
 
 test_that("basis() works with a gam", {
@@ -21,7 +22,7 @@ test_that("basis() works with a gam", {
 
     expect_silent(bs <- basis(m_gam, "s(x2)"))
     expect_s3_class(bs, "basis")
-    expect_named(bs, c("smooth", "type", "by_variable", "bf", "value", "x2"))
+    expect_named(bs, c(bs_nms, "x2"))
 
     plt2 <- draw(bs)
 
@@ -34,7 +35,7 @@ test_that("basis() works with a scam", {
     skip_on_cran()
     expect_silent(bs <- basis(m_scam, "s(x2)"))
     expect_s3_class(bs, "basis")
-    expect_named(bs, c("smooth", "type", "by_variable", "bf", "value", "x2"))
+    expect_named(bs, c(bs_nms, "x2"))
 
     plt <- draw(bs)
 
