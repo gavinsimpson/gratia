2339ca274a5ed4c29d0729b66b7a8e26870aa519
Gavin Simpson <ucfagls@gmail.com>
2024-03-25 18:01:20 +0100

    skip gfam stuff on version of mgcv < 1.9-0

--- tests/testthat/test-gfam.R
+++ tests/testthat/test-gfam.R
@@ -1,5 +1,6 @@
 # testing gratia's ability to handle gfam models - not much yet
 test_that("gfam data sim and model works", {
+  skip_if_not_installed("mgcv", "1.9-0")
   expect_silent(
     gfam_df <- data_sim("gfam", n = 400, seed = 2,
       gfam_families = c("binary", "tweedie", "normal"))
