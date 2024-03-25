b48d0376c64eaae9b59cbd313e9da1976ba42c81
Gavin Simpson <ucfagls@gmail.com>
2024-03-25 09:36:59 +0100

    clean up

--- R/by-variable-utilities.R
+++ R/by-variable-utilities.R
@@ -56,35 +56,6 @@
   )
 }
 
-## !! Only affects evaluate_smooth() which was removed in 0.9.0
-#' @importFrom tibble add_column
-`add_by_var_info_to_smooth` <- function(smooth, by_name, by_data, n) {
-  nc <- NCOL(smooth)
-  nr <- NROW(smooth)
-  if (length(by_name) > 1L) {
-    if (by_name[[1L]] == "NA") {
-      f <- factor(c(rep(NA_character_, n), rep(by_name[[2L]], NROW(smooth) - n)))
-      d <- factor(rep(c(NA, levels(by_data)), each = n))
-    } else {
-      f <- factor(c(rep(by_name[[2L]], NROW(smooth) - n), rep(NA_character_, n)))
-      d <- factor(rep(c(levels(by_data), NA), each = n))
-    }
-    smooth <- add_column(smooth, by_variable = f, .after = 1L)
-    smooth <- add_column(smooth, by_var = d)
-    names(smooth)[NCOL(smooth)] <- by_name[by_name != "NA"]
-  } else {
-    smooth <- add_column(smooth,
-      by_variable = factor(rep(by_name, NROW(smooth))),
-      .after = 1L
-    )
-    smooth <- add_column(smooth,
-      by_var = factor(rep(levels(by_data), each = n))
-    )
-    names(smooth)[NCOL(smooth)] <- by_name
-  }
-  smooth
-}
-
 ## Adds data to an object in a consistent way
 ##
 ## Variables in `data` are selected using `vars`. Then they are renamed
