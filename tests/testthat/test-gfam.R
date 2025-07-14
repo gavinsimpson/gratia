dabe7775abc35d4de899b4a78722d15ceda5d338
Gavin Simpson <ucfagls@gmail.com>
2025-07-14 15:24:28 +0200

    add example test of a two species beg bin model with different theta

--- tests/testthat/test-gfam.R
+++ tests/testthat/test-gfam.R
@@ -11,3 +11,38 @@ test_that("gfam data sim and model works", {
       data = gfam_df, method = "REML")
   )
 })
+
+# test something like two species responding to same covariate, but one with
+# very different variation
+test_that("gratia can handle a two species negbin model with gfam", {
+  # Number of data per species
+  N <- 200
+  ## simulate first species
+  gfam_sp1 <- data_sim(
+    "gwf2", scale = 1.5, theta = 2, # lower scale and theta
+    n = N, seed = 25, dist = "negbin"
+  )
+  # simulate species 2:, larger theta and scale
+  gfam_sp2 <- data_sim(
+    "gwf2", scale = 2, theta = 4, # larger scale and theta
+    n = N, seed = 25, dist = "negbin"
+  )
+  # bind together
+  gfam_df2 <- gfam_sp1 |>
+    dplyr::bind_rows(gfam_sp2) |>
+    dplyr::mutate(spp = rep(c(1, 2), each = N), f_spp = factor(spp))
+  # fit the model
+  two_sp_gfam <- gam(
+    cbind(y, spp) ~ f_spp + s(x, by = f_spp),
+    data = gfam_df,
+    method = "REML",
+    family = gfam(list("nb", "nb"))
+  )
+  fam <- family(two_sp_gfam)
+  expect_identical(
+    fam$getTheta(),
+    theta(two_sp_gfam)
+  )
+})
+
+# needs more tests
