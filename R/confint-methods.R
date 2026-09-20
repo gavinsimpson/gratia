9a637a4db0f4eb1efa02251caa8fa083b417f6c8
Gavin Simpson <ucfagls@gmail.com>
2026-09-20 10:19:35 +0200

    Fix simultaneous intervals for multiple smooths by selecting the current smooth within each iteration (GRA-006).

--- R/confint-methods.R
+++ R/confint-methods.R
@@ -408,7 +408,7 @@
       # else branch as smooth_estimates knows how to handle very specific
       # smooth names FIXME
       if (is.null(by_levs)) { # not by variable smooth
-        smooth <- get_smooth(object, parm) # get the specific smooth
+        smooth <- get_smooth(object, uS[i]) # get the specific smooth
         crit <- sim_interval(smooth, level = level, data = out[[i]])
       } else { # is a by variable smooth
         smooth <- old_get_smooth(object, uS[i])
